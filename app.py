0efe82866dfa2e8710bf917a9b94a40b3c61d702
saksak2003-coder <saksak2003@gmail.com>
2026-03-20 17:20:37 +0300

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Привет! Это мое приложение для упражнения 1.15!"
+    return "Привет! Это мое приложение для упражнения 1.16!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
